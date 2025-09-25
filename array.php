156304976f15b9262e37e33d69cda363e15505bc
RafifFarrelsyahFawwazka <rafifcer123@gmail.com>
2025-09-25 08:12:47 +0700

    Add files via upload

---
+++ array.php
@@ -0,0 +1,67 @@
+<?php
+$nilaiSiswa = [85, 92, 78, 64, 90, 55, 88, 79, 70, 96];
+
+$nilaiLulus = [];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai >= 70) {
+        $nilaiLulus[] = $nilai;
+    }
+}
+
+echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+echo"<br><br>";
+
+
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+echo "<h3>Daftar Karyawan dan Pengalaman Kerja</h3>";
+foreach ($daftarKaryawan as $karyawan) {
+    echo $karyawan[0] . " - " . $karyawan[1] . " tahun<br>";
+
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "<br><b>Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun:</b> " 
+     . implode(', ', $karyawanPengalamanLimaTahun) . "<br>";
+
+echo "Total: " . count($karyawanPengalamanLimaTahun) . " orang";
+echo"<br><br>";
+
+
+$daftarNilai = [
+    'Matematika' => [
+        ['Alice', 85],
+        ['Bob', 92],
+        ['Charlie', 78],
+    ],
+    'Fisika' => [
+        ['Alice', 90],
+        ['Bob', 88],
+        ['Charlie', 75],
+    ],
+    'Kimia' => [
+        ['Alice', 92],
+        ['Bob', 80],
+        ['Charlie', 85],
+    ],
+];
+
+$mataKuliah = 'Fisika';
+
+echo "Daftar nilai mahasiswa dalam mata kuliah $mataKuliah: <br>";
+
+foreach ($daftarNilai[$mataKuliah] as $nilai) {
+    echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+}
+?>
